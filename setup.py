d702f8c2b7f36831d6f3b8cfb696ea5a98c9d289
Shoma Furuya <s22c1116mr@s.chibakoudai.jp>
2023-12-03 03:15:01 +0900

    commit

--- setup.py
+++ setup.py
@@ -15,11 +15,13 @@ setup(
     zip_safe=True,
     maintainer='tom1513',
     maintainer_email='s22c1116mr@s.chibakoudai.jp',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='a package for practice',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
         ],
     },
 )
